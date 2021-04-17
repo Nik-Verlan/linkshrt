a6b35d2238fb11f475b153e2ca02e2d9971c4fd5
Nikita <lanfor96@yandex.by>
2021-04-17 15:58:53 +0300

    linkshrt

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import main, create, redirect_clicks, delete
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', main),
     path('create/', create),
     path('delete/', delete),
     path('<str:url_hash>/', redirect_clicks, name='redirect_clicks'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
